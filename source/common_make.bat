2f996d0e9b1424216a9eb1134fa065845adb6d6c
guerra000 <guerra000@06a8d622-d8fc-4012-a104-e4fd1d618cae>
2007-09-13 03:44:31 +0000

    2007-09-12 22:41 UTC-0500 Vicente Guerra <vicente@guerra.com.mx>   * source/common_make.bat   + source/c_activex.c   * source/h_activex.prg   * source/makelib_bcc.bat   * source/makelib_mingw.bat     * ActiveX splitted in h_* and c_* files for compilation warnings       when _HB_API_INTERNAL_ is defined.

--- source/common_make.bat
+++ source/common_make.bat
@@ -1,6 +1,6 @@
 @echo off
 rem
-rem $Id: common_make.bat,v 1.7 2007-07-15 04:48:43 guerra000 Exp $
+rem $Id: common_make.bat,v 1.8 2007-09-13 03:44:31 guerra000 Exp $
 rem
 
 if "%1"=="" goto INFO
@@ -9,7 +9,7 @@ rem /// File list
 
 SET HG_FILES1_PRG=h_browse h_scrsaver h_error h_ipaddress h_monthcal h_help h_crypt h_status h_tree h_toolbar h_init h_media h_winapimisc h_slider h_button h_checkbox h_combo h_controlmisc h_datepicker h_editbox h_dialogs h_grid h_windows h_image h_label h_listbox h_menu h_msgbox h_frame h_progressbar h_radio h_spinner h_tab h_textbox h_timer h_cursor h_ini h_report h_registry h_font h_hyperlink h_hotkey h_graph h_richeditbox h_edit h_edit_ex h_zip
 SET HG_FILES2_PRG=h_http h_comm h_print h_scroll h_splitbox h_progressmeter h_scrollbutton h_xbrowse h_internal h_textarray h_hotkeybox h_activex h_pdf
-SET HG_FILES_C=c_ipaddress c_monthcal c_help c_crypt c_tree c_media c_controlmisc c_datepicker c_resource c_cursor c_ini c_font c_dialogs c_grid c_windows c_image c_listbox c_msgbox c_frame c_progressbar c_spinner c_textbox c_timer c_winapimisc c_scrsaver c_graph
+SET HG_FILES_C=c_ipaddress c_monthcal c_help c_crypt c_tree c_media c_controlmisc c_datepicker c_resource c_cursor c_ini c_font c_dialogs c_grid c_windows c_image c_listbox c_msgbox c_frame c_progressbar c_spinner c_textbox c_timer c_winapimisc c_scrsaver c_graph c_activex
 
 rem /// Checks Harbour/xHarbour
 
--- source/makelib_mingw.bat
+++ source/makelib_mingw.bat
@@ -1,6 +1,6 @@
 @echo off
 rem
-rem $Id: makelib_mingw.bat,v 1.7 2007-07-15 04:48:43 guerra000 Exp $
+rem $Id: makelib_mingw.bat,v 1.8 2007-09-13 03:44:31 guerra000 Exp $
 rem
 cls
 
@@ -24,7 +24,7 @@ SET OOHG_X_FLAGS=-Wall -mno-cygwin -O3 -c -I%hg_hrb%\include -I%hg_mgw%\include 
 SET _PATH2=%PATH%
 SET PATH=%hg_mgw%\bin;%PATH%
 
-gcc %OOHG_X_FLAGS% h_scrsaver.c h_edit.c h_edit_ex.c h_error.c h_ipaddress.c c_ipaddress.c h_monthcal.c c_monthcal.c h_help.c c_help.c h_crypt.c c_crypt.c h_status.c h_tree.c c_tree.c h_toolbar.c h_init.c h_media.c c_media.c h_winapimisc.c h_slider.c c_controlmisc.c c_datepicker.c c_resource.c h_cursor.c c_cursor.c c_ini.c h_ini.c h_report.c h_registry.c h_font.c c_font.c h_hyperlink.c h_richeditbox.c h_scroll.c h_http.c h_zip.c h_progressmeter.c h_comm.c h_print.c h_activex.c h_pdf.c
+gcc %OOHG_X_FLAGS% h_scrsaver.c h_edit.c h_edit_ex.c h_error.c h_ipaddress.c c_ipaddress.c h_monthcal.c c_monthcal.c h_help.c c_help.c h_crypt.c c_crypt.c h_status.c h_tree.c c_tree.c h_toolbar.c h_init.c h_media.c c_media.c h_winapimisc.c h_slider.c c_controlmisc.c c_datepicker.c c_resource.c h_cursor.c c_cursor.c c_ini.c h_ini.c h_report.c h_registry.c h_font.c c_font.c h_hyperlink.c h_richeditbox.c h_scroll.c h_http.c h_zip.c h_progressmeter.c h_comm.c h_print.c h_activex.c c_activex.c h_pdf.c
 if errorlevel 1 goto EXIT2
 gcc %OOHG_X_FLAGS% c_dialogs.c c_grid.c c_windows.c c_image.c c_listbox.c c_msgbox.c c_frame.c c_progressbar.c c_spinner.c c_textbox.c c_timer.c c_winapimisc.c h_button.c h_checkbox.c h_combo.c h_controlmisc.c h_datepicker.c h_editbox.c h_dialogs.c h_grid.c h_windows.c h_image.c h_label.c h_listbox.c h_menu.c h_msgbox.c h_frame.c h_progressbar.c h_radio.c h_spinner.c h_tab.c h_textbox.c h_timer.c c_scrsaver.c h_hotkey.c h_graph.c c_graph.c h_browse.c h_splitbox.c h_scrollbutton.c h_xbrowse.c h_internal.c h_textarray.c h_hotkeybox.c
 if errorlevel 1 goto EXIT2
@@ -33,7 +33,7 @@ if errorlevel 1 goto EXIT2
 if exist miniprint.c gcc %OOHG_X_FLAGS% miniprint.c
 if errorlevel 1 goto EXIT2
 
-%hg_mgw%\bin\ar rc %hg_root%\lib\liboohg.a h_scrsaver.o h_edit.o h_edit_ex.o h_error.o h_ipaddress.o c_ipaddress.o h_monthcal.o c_monthcal.o h_help.o c_help.o h_status.o h_tree.o c_tree.o h_toolbar.o h_init.o h_media.o c_media.o c_resource.o h_cursor.o c_cursor.o h_ini.o c_ini.o h_report.o h_font.o c_font.o h_hyperlink.o c_scrsaver.o h_hotkey.o h_graph.o c_graph.o h_richeditbox.o h_browse.o h_scroll.o h_http.o h_zip.o h_progressmeter.o h_comm.o h_print.o h_splitbox.o h_scrollbutton.o h_xbrowse.o h_activex.o h_pdf.o
+%hg_mgw%\bin\ar rc %hg_root%\lib\liboohg.a h_scrsaver.o h_edit.o h_edit_ex.o h_error.o h_ipaddress.o c_ipaddress.o h_monthcal.o c_monthcal.o h_help.o c_help.o h_status.o h_tree.o c_tree.o h_toolbar.o h_init.o h_media.o c_media.o c_resource.o h_cursor.o c_cursor.o h_ini.o c_ini.o h_report.o h_font.o c_font.o h_hyperlink.o c_scrsaver.o h_hotkey.o h_graph.o c_graph.o h_richeditbox.o h_browse.o h_scroll.o h_http.o h_zip.o h_progressmeter.o h_comm.o h_print.o h_splitbox.o h_scrollbutton.o h_xbrowse.o h_activex.o c_activex.o h_pdf.o
 if errorlevel 2 goto EXIT2
 %hg_mgw%\bin\ar rc %hg_root%\lib\liboohg.a c_crypt.o h_crypt.o h_winapimisc.o h_slider.o c_controlmisc.o c_datepicker.o c_dialogs.o c_grid.o c_windows.o c_image.o c_listbox.o c_msgbox.o c_frame.o c_progressbar.o c_spinner.o c_textbox.o c_timer.o c_winapimisc.o h_button.o h_checkbox.o h_combo.o h_controlmisc.o h_datepicker.o h_editbox.o h_dialogs.o h_grid.o h_windows.o h_image.o h_label.o h_listbox.o h_menu.o h_msgbox.o h_frame.o h_progressbar.o h_radio.o h_spinner.o h_tab.o h_textbox.o h_timer.o h_registry.o h_internal.o h_textarray.o h_hotkeybox.o
 if errorlevel 2 goto EXIT2
