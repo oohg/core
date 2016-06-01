97b3dc344e2a2ca2dd29aa4f4fe7d04a0f7e943f
Fernando Yurisich <fernando.yurisich@gmail.com>
2016-06-01 00:50:37 +0000

    2016-05-31 21:51 UTC-0300 Fernando Yurisich <fyurisich@oohg.org>    * ide/build.bat      + Logic to handle HB30 and HB32 in the same instalation.        Use 'BUILD.BAT' HB32 for incremental compilation with HB32        Use 'BUILD.BAT' HB30 for incremental compilation with HB30        Use 'BUILD.BAT' /C HB32 for full compilation with HB32        Use 'BUILD.BAT' /C HB30 for full compilation with HB30        /C swith is recommended to avoid mixing version.    * manual/Changelog.htm      * Updated.

--- ide/build.bat
+++ ide/build.bat
@@ -1,11 +1,49 @@
 @echo off
 rem
-rem $Id: build.bat,v 1.2 2014-04-15 00:46:19 fyurisich Exp $
+rem $Id: build.bat,v 1.3 2016-06-01 00:50:37 fyurisich Exp $
 rem
 cls
 
-rem *** Set Paths ***
+:PATH
+if /I "%1"=="/C" goto CLEAN_PATH
 if "%HG_ROOT%"=="" set HG_ROOT=c:\oohg
+set HG_CLEAN=
+goto PARAMS
+
+:CLEAN_PATH
+set HG_ROOT=c:\oohg
+set HG_CLEAN=/C
+shift
+
+:PARAMS
+if /I "%1"=="HB30" goto CALL30
+if /I "%1"=="HB32" goto CALL32
+
+:NOVERSION
+if exist %HG_ROOT%\buildapp30.bat goto CONTINUE
+if exist %HG_ROOT%\buildapp32.bat goto CALL32
+echo File %HG_ROOT%\buildapp30.bat not found !!!
+echo File %HG_ROOT%\buildapp32.bat not found !!!
+echo.
+goto END
+
+:CONTINUE
+if not exist %HG_ROOT%\buildapp32.bat goto CALL30
+echo Syntax:
+echo    To build with Harbour 3.0
+echo       build [/C] HB30
+echo   To build with Harbour 3.2
+echo       build [/C] HB32
+echo.
+goto END
 
 rem *** Call Compiler Specific Batch File ***
-call %HG_ROOT%\buildapp.bat mgide %1 %2
+:CALL30
+call %HG_ROOT%\buildapp.bat %HG_CLEAN% HB30 mgide %2 %3
+goto END
+
+:CALL32
+call %HG_ROOT%\buildapp.bat %HG_CLEAN% HB32 mgide %2 %3
+goto END
+
+:END
