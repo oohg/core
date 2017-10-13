3f2ff17eeafac56521842c644530cfdd8087d8df
Fernando Yurisich <fernando.yurisich@gmail.com>
2017-10-12 21:48:25 -0300

    2017-10-12 20:47:00 UTC-0300 Fernando Yurisich <fyurisich@oohg.org>
    - samples/dbview/*.*
    - samples/http/*.*
    - samples/mpm/*.*
    - samples/oohg/*.*
    - ./samples
    ; Moved to a separated repository.

--- samples/http/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem
-rem $Id: build.bat,v 1.1 2015-10-28 02:23:00 fyurisich Exp $
-rem
-
-set THR_LIB=-liphlpapi
-compile httpdemo
--- samples/oohg/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem
-rem $Id: build.bat,v 1.3 2013-11-13 18:06:43 migsoft Exp $
-rem
-
-SET DISCO=%~d1
-SET OOHGPATH=%DISCO%\oohg
-
-%OOHGPATH%\2buildapp.bat %*
