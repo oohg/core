7ea0a5da1336e173b367be041a1f1c4bba72ecc3
José M. C. Quintas <jmcquintas@gmail.com>
2017-10-31 10:34:55 -0200

    step 1: internal names on buildlib.bat, buildlib30.bat, buildlib32.bat

--- source/BuildLib.bat
+++ source/BuildLib.bat
@@ -6,8 +6,8 @@ rem
 :MAIN
 
    cls
-   if /I "%1"=="HB30" goto CHECK30
-   if /I "%1"=="HB32" goto CHECK32
+   if /I "%1"=="HB30" goto :CHECK30
+   if /I "%1"=="HB32" goto :CHECK32
 
 :NOVERSION
 
@@ -18,47 +18,47 @@ rem
    echo.
    echo This file must be executed from SOURCE folder !!!
    echo.
-   goto END
+   goto :END
 
 :CONTINUE
 
-   if not exist buildlib32.bat goto HB30
+   if not exist buildlib32.bat goto :HB30
    echo Syntax:
    echo    To build with Harbour 3.0
    echo       buildlib HB30 [options]
    echo   To build with Harbour 3.2
    echo       buildlib HB32 [options]
    echo.
-   goto END
+   goto :END
 
 :CHECK30
 
    shift
-   if exist buildlib30.bat goto HB30
+   if exist buildlib30.bat goto :HB30
    echo File buildlib30.bat not found !!!
    echo.
    echo This file must be executed from SOURCE folder !!!
    echo.
-   goto END
+   goto :END
 
 :CHECK32
 
    shift
-   if exist buildlib32.bat goto HB32
+   if exist buildlib32.bat goto :HB32
    echo File buildlib32.bat not found !!!
    echo.
    echo This file must be executed from SOURCE folder !!!
    echo.
-   goto END
+   goto :END
 
 :HB30
 
    call buildlib30.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
-   goto END
+   goto :END
 
 :HB32
 
    call buildlib32.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
-   goto END
+   goto :END
 
 :END
--- source/BuildLib30.bat
+++ source/BuildLib30.bat
@@ -3,22 +3,24 @@ rem
 rem $Id: BuildLib30.bat,v 1.1 2015-03-14 01:11:45 fyurisich Exp $
 rem
 
-:MAIN
+rem --- build30.bat
+
+:HB30MAIN
 
    cls
 
    rem *** Set Paths ***
-   if "%1"=="/C" goto CLEAN_PATH
-   if "%1"=="/c" goto CLEAN_PATH
+   if "%1"=="/C" goto :HB30CLEAN_PATH
+   if "%1"=="/c" goto :HB30CLEAN_PATH
    if "%HG_ROOT%"==""  set HG_ROOT=c:\oohg
    if "%HG_HRB%"==""   set HG_HRB=c:\oohg\hb30
    if "%HG_MINGW%"=="" set HG_MINGW=c:\oohg\hb30\comp\mingw
    if "%LIB_GUI%"==""  set LIB_GUI=lib
    if "%LIB_HRB%"==""  set LIB_HRB=lib
    if "%BIN_HRB%"==""  set BIN_HRB=bin
-   goto BUILD
+   goto :HB30BUILD
 
-:CLEAN_PATH
+:HB30CLEAN_PATH
 
    set HG_ROOT=c:\oohg
    set HG_HRB=c:\oohg\hb30
@@ -28,9 +30,12 @@ rem
    set BIN_HRB=bin
    shift
 
-:BUILD
+:HB30BUILD
 
    set HG_CCOMP=%HG_MINGW%
    set HBMK2_WORDIR=-workdir=%HG_ROOT%\%LIB_GUI%\.hbmk
    call BuildLib_hbmk2.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
    set HBMK2_WORDIR=
+   goto :END
+
+:END
