c2b9c606bba3e592bd2786a00853a5088790b732
Fernando Yurisich <fyurisich@oohg.org>
2017-11-02 18:16:57 -0300

    2017-11-02 18:16 UTC-0300 Fernando Yurisich <fyurisich@oohg.org>
       * core\source\makelib_bcc.bat
         * Command's output cleaned.
         ! Label not found error.

--- source/makelib_bcc.bat
+++ source/makelib_bcc.bat
@@ -17,73 +17,65 @@ rem
    if "%BIN_HRB%"=="" set BIN_HRB=bin
 
    rem *** Create Lib Folder ***
-   if not exist %HG_ROOT%\%LIB_GUI%\nul md %HG_ROOT%\%LIB_GUI%
+   if not exist %HG_ROOT%\%LIB_GUI%\nul md %HG_ROOT%\%LIB_GUI% > nul
 
    rem *** Delete Old Libraries ***
-   if exist %HG_ROOT%\%LIB_GUI%\oohg.lib      del %HG_ROOT%\%LIB_GUI%\oohg.lib
-   if exist %HG_ROOT%\%LIB_GUI%\hbprinter.lib del %HG_ROOT%\%LIB_GUI%\hbprinter.lib
-   if exist %HG_ROOT%\%LIB_GUI%\miniprint.lib del %HG_ROOT%\%LIB_GUI%\miniprint.lib
-   if exist %HG_ROOT%\%LIB_GUI%\bostaurus.lib del %HG_ROOT%\%LIB_GUI%\bostaurus.lib
+   if exist %HG_ROOT%\%LIB_GUI%\oohg.lib      del %HG_ROOT%\%LIB_GUI%\oohg.lib      > nul
+   if exist %HG_ROOT%\%LIB_GUI%\hbprinter.lib del %HG_ROOT%\%LIB_GUI%\hbprinter.lib > nul
+   if exist %HG_ROOT%\%LIB_GUI%\miniprint.lib del %HG_ROOT%\%LIB_GUI%\miniprint.lib > nul
+   if exist %HG_ROOT%\%LIB_GUI%\bostaurus.lib del %HG_ROOT%\%LIB_GUI%\bostaurus.lib > nul
 
    rem *** Compile with Harbour ***
    echo Compiling prg files ...
    if exist resul.txt del resul.txt
    call common_make "%HG_HRB%\%LIB_HRB%\tip.lib" -q0 ">> resul.txt 2>&1"
-   if errorlevel 1 goto EXIT1
+   if errorlevel 1 goto ERROR1
    echo. >> resul.txt
 
    rem *** Compile with BCC ***
    echo Compiling c files ...
    set OOHG_X_FLAGS=-c -O2 -tW -tWM -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; -D__XHARBOUR__
    for %%a in ( %HG_FILES1_PRG% ) do if not errorlevel 1 %HG_BCC%\bin\bcc32 %OOHG_X_FLAGS% %%a.c >> resul.txt
-   if errorlevel 1 goto EXIT2
+   if errorlevel 1 goto ERROR2
    for %%a in ( %HG_FILES2_PRG% ) do if not errorlevel 1 %HG_BCC%\bin\bcc32 %OOHG_X_FLAGS% %%a.c >> resul.txt
-   if errorlevel 1 goto EXIT2
+   if errorlevel 1 goto ERROR2
    for %%a in ( %HG_FILES_C% )    do if not errorlevel 1 %HG_BCC%\bin\bcc32 %OOHG_X_FLAGS% %%a.c >> resul.txt
-   if errorlevel 1 goto EXIT2
+   if errorlevel 1 goto ERROR2
    if exist winprint.c  %HG_BCC%\bin\bcc32 %OOHG_X_FLAGS% winprint.c >> resul.txt
-   if errorlevel 1 goto EXIT2
+   if errorlevel 1 goto ERROR2
    if exist miniprint.c %HG_BCC%\bin\bcc32 %OOHG_X_FLAGS% miniprint.c >> resul.txt
-   if errorlevel 1 goto EXIT2
+   if errorlevel 1 goto ERROR2
    if exist bostaurus.c %HG_BCC%\bin\bcc32 %OOHG_X_FLAGS% bostaurus.c >> resul.txt
-   if errorlevel 1 goto EXIT2
+   if errorlevel 1 goto ERROR2
    echo. >> resul.txt
 
    rem *** Build Libraries ***
    echo Building libraries ...
    for %%a in ( %HG_FILES1_PRG% ) do if not errorlevel 2 %HG_BCC%\bin\tlib %HG_ROOT%\%LIB_GUI%\oohg +%%a.obj /P32 >> resul.txt
-   if errorlevel 2 goto EXIT3
+   if errorlevel 2 goto ERROR2
    for %%a in ( %HG_FILES2_PRG% ) do if not errorlevel 2 %HG_BCC%\bin\tlib %HG_ROOT%\%LIB_GUI%\oohg +%%a.obj /P32 >> resul.txt
-   if errorlevel 2 goto EXIT3
+   if errorlevel 2 goto ERROR2
    for %%a in ( %HG_FILES_C% )    do if not errorlevel 2 %HG_BCC%\bin\tlib %HG_ROOT%\%LIB_GUI%\oohg +%%a.obj /P32 >> resul.txt
-   if errorlevel 2 goto EXIT3
+   if errorlevel 2 goto ERROR2
    if exist winprint.obj  %HG_BCC%\bin\tlib %HG_ROOT%\%LIB_GUI%\hbprinter +winprint.obj >> resul.txt
-   if errorlevel 2 goto EXIT3
+   if errorlevel 2 goto ERROR2
    if exist miniprint.obj %HG_BCC%\bin\tlib %HG_ROOT%\%LIB_GUI%\miniprint +miniprint.obj >> resul.txt
-   if errorlevel 2 goto EXIT3
+   if errorlevel 2 goto ERROR2
    if exist bostaurus.obj %HG_BCC%\bin\tlib %HG_ROOT%\%LIB_GUI%\bostaurus +bostaurus.obj >> resul.txt
-   if errorlevel 2 goto EXIT3
+   if errorlevel 2 goto ERROR2
 
-:EXIT3
-
-   rem *** Cleanup ***
-   IF EXIST %HG_ROOT%\%LIB_GUI%\oohg.bak      del %HG_ROOT%\%LIB_GUI%\oohg.bak
-   IF EXIST %HG_ROOT%\%LIB_GUI%\hbprinter.bak del %HG_ROOT%\%LIB_GUI%\hbprinter.bak
-   IF EXIST %HG_ROOT%\%LIB_GUI%\miniprint.bak del %HG_ROOT%\%LIB_GUI%\miniprint.bak
-   IF EXIST %HG_ROOT%\%LIB_GUI%\bostaurus.bak del %HG_ROOT%\%LIB_GUI%\bostaurus.bak
-
-:EXIT2
+:ERROR2
 
    rem *** Cleanup ***
-   del *.obj
+   del /q *.obj > nul
 
-:EXIT1
+:ERROR1
 
    rem *** Cleanup ***
-   del h_*.c
-   if exist winprint.c  del winprint.c
-   if exist miniprint.c del miniprint.c
-   if exist bostaurus.c del bostaurus.c
+   del /q h_*.c > nul
+   if exist winprint.c  del winprint.c  > nul
+   if exist miniprint.c del miniprint.c > nul
+   if exist bostaurus.c del bostaurus.c > nul
    SET OOHG_X_FLAGS=
    SET HG_FILES1_PRG=
    SET HG_FILES2_PRG=
@@ -91,7 +83,7 @@ rem
 
 :SHOW_RESULT
 
-   if not exist resul.txt goto EXIT
+   if not exist resul.txt goto END
    type resul.txt | more
 
-:EXIT
+:END
