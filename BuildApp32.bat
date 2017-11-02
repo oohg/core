501a49297d4d87b02e8b6d91168e68990d4e3a9b
José M. C. Quintas <jmcquintas@gmail.com>
2017-11-02 13:08:45 -0200

    hb30 & hb32 was inverse

--- BuildApp32.bat
+++ BuildApp32.bat
@@ -1,3 +1,3 @@
 @echo off
-if /I "%1"=="/c" buildapp %1 HB30 %2 %3 %4 %5 %6 %7 %8 %9
-if not /I "%1"=="/c" buildapp HB30 %1 %2 %3 %4 %5 %6 %7 %8 %9
+if /I "%1"=="/c" buildapp %1 HB32 %2 %3 %4 %5 %6 %7 %8 %9
+if not /I "%1"=="/c" buildapp HB32 %1 %2 %3 %4 %5 %6 %7 %8 %9
